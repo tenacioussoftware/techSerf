aea7e8c7f38d16b046a6c15f13d60601320eae7d
Erik Powell <epowell@theerikpowell.com>
2015-11-27 17:54:59 -0500

    Added Bootstrap base files and got the _layout.cshtml file created. Further templating to follow.

--- techSerf/Bootstrapper.cs
+++ techSerf/Bootstrapper.cs
@@ -7,5 +7,13 @@
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            //Nancy.ViewEngines.Razor.RazorViewEngine
+            var engine = container.Resolve<Nancy.ViewEngines.Razor.RazorViewEngine>();
+
+        }
     }
 }
