fe646968385dd7176d8672d7e1191b696c790a83
Erik Powell <epowell@theerikpowell.com>
2015-11-27 22:51:59 -0500

    Added Admin area, integrated SB Admin template, and modified the BootStrapper to handle Areas so we can separate Content& Scripts by module.

--- techSerf/Areas/Admin/AdminModule.cs
+++ techSerf/Areas/Admin/AdminModule.cs
@@ -1,11 +1,17 @@
-﻿using Nancy;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
 using techSerfWeb.Properties;
 
-namespace techSerfWeb
+namespace techSerfWeb.Areas.Admin
 {
-    public class IndexModule : NancyModule
+    public class AdminModule: NancyModule
     {
-        public IndexModule()
+        public AdminModule()
+            : base ("/admin")
         {
             //Things to process before the route
             Before += ctx =>
@@ -13,17 +19,13 @@ namespace techSerfWeb
                 //Grab any settings info and throw it into ViewBag
                 Settings s = new Settings();
                 ViewBag.ApplicationName = s.appName;
+                ViewBag.ViewBase = "Areas/Admin/Views/";
                 return null; //Return null to proceed, return HtmlResponse to abort request.
             };
 
             Get["/"] = parameters =>
             {
                 return View["index"];
-            };
-
-            Get["/about"] = parameters =>
-            {
-                return View["about"];
             };
         }
 
